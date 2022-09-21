414487ac7bdcaae6eef23e83093a37b00a0f5b6d
Isak Naslund <Isak.Naslund@burohappold.com>
2022-09-21 11:12:07 +0200

    Adding enum for how the dataset data should be queried for default import ratios

---
+++ EmbodiedSuffering_oM/Elements/Enums/ImportSourceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace BH.oM.EmbodiedSuffering.Elements
+{
+    [Description("Enum describing if the ratios for datasets used should be by mass or by cost.")]
+    public enum ImportSourceType
+    {
+        Undefined,
+        ByCost,
+        ByMass
+    }
+}
